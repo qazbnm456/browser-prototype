7dec4bfc57e578f35a5e468b25761012872d7c14
Boik <qazbnm456@gmail.com>
2017-07-12 12:53:35 +0800

    adjust the structure of the project, and add the first child component "Page" under BrowserMainView

--- src/renderer/components/BrowserMainView.vue
+++ src/renderer/components/BrowserMainView.vue
@@ -1,45 +1,15 @@
 <template>
-  <div id="wrapper">
-    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
-    <main>
-      <div class="left-side">
-        <span class="title">
-          Welcome to your new project!
-        </span>
-        <system-information></system-information>
-      </div>
-
-      <div class="right-side">
-        <div class="doc">
-          <div class="title">Getting Started</div>
-          <p>
-            electron-vue comes packed with detailed documentation that covers everything from
-            internal configurations, using the project structure, building your application,
-            and so much more.
-          </p>
-          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
-        </div>
-        <div class="doc">
-          <div class="title alt">Other Documentation</div>
-          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
-          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
-        </div>
-      </div>
-    </main>
+  <div>
+    <page></page>
   </div>
 </template>
 
 <script>
-  import SystemInformation from './BrowserMainView/SystemInformation';
+  import Page from './BrowserMainView/Page';
 
   export default {
     name: 'browser-main-view',
-    components: { SystemInformation },
-    methods: {
-      open(link) {
-        this.$electron.shell.openExternal(link);
-      },
-    },
+    components: { Page },
   };
 </script>
 
---
+++ src/renderer/components/BrowserMainView/Page.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <webview ref="webview"></webview>
+  </div>
+</template>
+
+<script>
+  export default {
+    mounted() {
+      const webview = this.$refs.webview;
+      if (webview) {
+        webview.setAttribute('src', 'https://github.com/qazbnm456/browser-prototype');
+      }
+    },
+  };
+</script>
+
+<style scoped>
+  webview {
+    height: 100vh;
+    width: 100vw;
+    outline: none;
+    position: relative;
+  }
+  webview[hidden] {
+    flex: 0 1;
+    width: 0px;
+    height: 0px !important;
+  }
+  webview.fullscreen {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    z-index: 99;
+  }
+  /* hack to work around display: none issues with webviews */
+  webview.hidden {
+    flex: 0 1;
+    width: 0px;
+    height: 0px !important;
+  }
+</style>
